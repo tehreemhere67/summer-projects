cf4c43fd319744ac3998cfa69a533348258d4da1
tehreem <tehreemhere67@gmail.com>
2026-06-19 17:34:56 +0500

    update csv explorer readme

---
+++ 07-csv-explorer/README.d
@@ -0,0 +1,8 @@
+# csv-explorer
+Here's what the program does:
+
+Load the CSV with pandas
+Show basic stats — average IMDB score, highest grossing movie, most common director
+Filter movies by minimum IMDB score
+Find top 10 movies by gross revenue
+Show movies by a specific director
